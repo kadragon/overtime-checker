37870e8254208569863c7c305615534f83397226
kadragon <kangdongouk@gmail.com>
2024-09-23 13:04:53 +0900

    feat: Add functionality to create weekly sheet and process overtime approval file
    Updated `main.py` to include options for user to create a weekly sheet or process
    the overtime approval file. Added `weekpage_maker.py` to create a weekly sheet
    and `overtime_maker.py` to process the overtime approval file. Also updated 
    the requirements in `requirements.txt` to include `gspread`.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,19 @@
-from utils.file_utils import find_target_excel_file, convert_xls_to_xlsx, create_meal_expense_file
-from utils.overtime_utils import check_overtime_pay, overtimeCnt, officialDataMaker
+from processors.weekpage_maker import create_weekly_sheet
+from processors.overtime_maker import create_overtime_file
 
 
 def main():
-    # 초과근무승인 파일 찾기
-    overtimeListFileName = find_target_excel_file("초과근무승인")
+    print('''
+          [1] 주간업무계획 생성
+          [2] 초과근무승인 파일 처리
+          ''')
+    print("번호를 입력하세요: ")
+    num = input()
 
-    # 엑셀 파일 변환
-    convertedExcelFileName = convert_xls_to_xlsx(overtimeListFileName)
-
-    # 초과근무 검토 파일 생성
-    overtimeFileName = create_meal_expense_file(convertedExcelFileName)
-
-    # 초과근무 조교 / 사전보고 등 검토
-    check_overtime_pay(overtimeFileName)
-
-    # 초과근무 월집계 파일 생성
-    overtimeNameCnt = overtimeCnt(overtimeFileName)
-
-    # 월 집계 파일 찾기
-    overtimeListFileName = find_target_excel_file("초과근무월집계")
-
-    # 엑셀 파일 변환
-    convertedExcelFileName = convert_xls_to_xlsx(overtimeListFileName)
-
-    # 공문 데이터 생성
-    officialDataMaker(convertedExcelFileName, overtimeNameCnt)
+    if num == "1":
+        create_weekly_sheet(1)
+    elif num == "2":
+        create_overtime_file()
 
 
 if __name__ == "__main__":
